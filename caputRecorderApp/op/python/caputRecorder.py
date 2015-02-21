7a93b06b6384da65167af696a270c9f3314c4ee8
timmmooney <mooney@aps.anl.gov>
2015-02-21 03:42:22 +0000

    Major changes for R1-4

--- caputRecorderApp/op/python/caputRecorder.py
+++ caputRecorderApp/op/python/caputRecorder.py
@@ -19,6 +19,7 @@ legalChars = string.letters + string.digits + "_"
 
 wake = threading.Event()
 debug = 0
+connected = True
 
 prefix = "xxx:"
 menuFields=["ZRST","ONST","TWST","THST","FRST","FVST","SXST","SVST","EIST","NIST","TEST","ELST","TVST","TTST","FTST","FFST"]
@@ -31,11 +32,16 @@ doexecuteMacro = 0
 doSelectMacro = 0
 doAbortMacro = 0
 executingMacro = 0
+exitProgram = 0
+
+# if execute while recording
+executeLevel = 0
+postponeStop = 0
 
 from inspect import getmembers, isfunction, getargspec
-maxArgs = 5
+maxArgs = 10
 
-maxMacroMenus = 2
+maxMacroMenus = 6
 
 # all macro functions
 _macroFunctionNames = []
@@ -48,6 +54,8 @@ macroFunctions = []
 MSG_COMMAND = 0
 MSG_COMMENT = 1
 MSG_DELAY = 2
+MSG_DO = 3
+
 
 allowedUsers = []
 allowedHosts = []
@@ -64,11 +72,11 @@ def makeCommandMonitorList(prefixStrings):
 	pstring = prefixStrings.translate(commaToSpaceTable)
 	userPrefixes = pstring.split(" ")
 	if debug: print "userPrefixes=", userPrefixes
-	if userPrefixes:
+	if userPrefixes and userPrefixes[0]:
 		commandMonitorList = []
 		for p in userPrefixes:
-			commandMonitorList.append(p+"caputRecorderCommand")
-	if debug: print "commandMonitorList=", commandMonitorList
+			if p: commandMonitorList.append(p+"caputRecorderCommand")
+	if debug: print "makeCommandMonitorList: commandMonitorList=", commandMonitorList
 
 ########################################################################
 ## Respond to monitors from command and comment PVs
@@ -119,6 +127,12 @@ def delayMonFunc(pvname, value, char_value, **kwd):
 	if value>0:
 		msgQueue.put((MSG_DELAY, char_value))
 
+## execute selected macro
+def executeMacroMonFunc(pvname, value, char_value, **kwd):
+	global debug, macroFile, msgQueue
+	if debug: print "executeMacroMonFunc: char_value=%s" % char_value
+	msgQueue.put((MSG_DO, char_value))
+
 ############################################################################
 ## writer thread
 # read from the message queue, and write to the macros.py file, which startMacro()
@@ -139,12 +153,18 @@ def userHostAllowed(user, host):
 		return False
 	return True
 
+
 def writer():
-	global debug, macroFile, prefix, msgQueue
+	global debug, macroFile, prefix, msgQueue, doexecuteMacro, executeLevel, postponeStop
+	global doStartMacro, doStopMacro, connected
 
 	while(1):
 		(msg, char_value) = msgQueue.get()
 		if debug: print "writer: type=%d, char_value='%s'" % (msg, char_value)
+		if executeLevel and (msg != MSG_DO):
+			continue
+		if not connected:
+			continue
 		if msg == MSG_COMMAND:
 			if char_value.find(prefix+"caputRecorder") == -1:
 				(pvname,value,user_host) = char_value.split(',')
@@ -168,24 +188,84 @@ def writer():
 			macroFile.write("\ttime.sleep(%.3f)\n" % delayTime)
 			macroFile.flush()
 			epics.caput(prefix+"caputRecorderAddDelayCmd", 0)
+		elif msg == MSG_DO:
+			if debug: print "writer:MSG_DO, char_value='%s'" % char_value
+			busy = epics.caget(prefix+"caputRecorderMacroRecording")
+			if char_value == "Do":
+				if (busy):
+					# add call to selected function to macro file
+					fname = epics.caget(prefix+"caputRecorderMacro")
+					cmd = "\t" + fname+"("
+					for j in range(1,maxArgs+1):
+						argName = epics.caget(prefix+("caputRecorderArg%dName" % j))
+						argValue = epics.caget(prefix+("caputRecorderArg%dValue" % j))
+						if argName:
+							if j>1:
+								cmd = cmd + ","
+							cmd = cmd + argName + "=" + argValue
+						else:
+							break
+					cmd = cmd + ")\n"
+					macroFile.write(cmd)
+					macroFile.flush()
+					# execute macro, but stop recording while it executes
+					executeLevel += 1
+					if executeLevel:
+						epics.caput(prefix+"caputRecorderUserMessage", "recording suppressed during execution")
+					doexecuteMacro = 1
+					wake.set()
+				else:
+					doexecuteMacro = 1
+					wake.set()
+			else:
+				if executeLevel > 0:
+					executeLevel -= 1
+					if executeLevel==0:
+						epics.caput(prefix+"caputRecorderUserMessage", "")
+						if postponeStop:
+							if debug: print "writer: executing postponed Stop"
+							doStopMacro = 1
+							postponeStop = 0
+							wake.set()
+
 
 ########################################################################
 ## Respond to monitors from StopStart PV
 
 def stopStartMonFunc(pvname, value, char_value, **kwd):
-	global debug, doStartMacro, doStopMacro
+	global debug, doStartMacro, doStopMacro, executingMacro, executeLevel, postponeStop
+	global connected
 
-	#print("stopStartMonFunc: value = ", value, "char_value = ", char_value)
-	if value:
-		doStartMacro = 1
+	if debug: print("stopStartMonFunc: value = ", value, "char_value = ", char_value)
+	if not connected:
+		return
+
+	if executeLevel > 0:
+		# ignore or postpone action on switch until executeLevel==0
+		if value:
+			epics.caput(prefix+"caputRecorderMacroStopStart", 0)
+			epics.caput(prefix+"caputRecorderUserMessage", "Can't start recording while executing")
+		else:
+			postponeStop = 1
+	elif executingMacro:
+		if value:
+			epics.caput(prefix+"caputRecorderMacroStopStart", 0)
+			epics.caput(prefix+"caputRecorderUserMessage", "Can't start recording while executing")
 	else:
-		doStopMacro = 1
-	wake.set()
+		if value:
+			doStartMacro = 1
+		elif executingMacro:
+			doStopMacro = 1
+		wake.set()
 
 def startMacro():
 	global debug, macroFile, prefix, macroFunctionNames
-	global commandMonitorList
+	global commandMonitorList, connected
+
 	if debug: print("startMacro: entry\n")
+	if not connected:
+		return
+
 	busy = epics.caget(prefix+"caputRecorderMacroRecording")
 	if (busy):
 		epics.caput(prefix+"caputRecorderUserMessage", "a macro is already being recorded")
@@ -207,6 +287,9 @@ def startMacro():
 			macroName += c
 		else:
 			macroName += "_"
+	# function name must be 25 characters or less (mbboRecord string length)
+	if len(macroName) > 25: macroName = macroName[:25]
+	# See of function name is already defined
 	if macroName in macroFunctionNames:
 		epics.caput(prefix+"caputRecorderUserMessage", "*** macro name is already in use")
 		epics.caput(prefix+"caputRecorderMacroStopStart", 0)
@@ -233,10 +316,14 @@ def startMacro():
 
 def endMacro():
 	global debug, macroFile, prefix, doReloadMacros
-	global commandMonitorList
-	#print("endMacro: entry\n")
-	busy = epics.caget(prefix+"caputRecorderMacroRecording")
-	if (not busy):
+	global commandMonitorList, connected
+
+	if debug: print("endMacro: entry\n")
+	if not connected:
+		return
+
+	recording = epics.caget(prefix+"caputRecorderMacroRecording")
+	if (not recording):
 		return
 
 	epics.camonitor_clear(prefix+"caputRecorderComment")
@@ -261,14 +348,21 @@ def endMacro():
 ## read macros from file, and write names to EPICS PVs
 
 def reloadMacrosMonFunc(pvname, value, char_value, **kwd):
-	global debug, doReloadMacros
+	global debug, doReloadMacros, connected
+
+	if not connected:
+		return
+
 	if value:
 		doReloadMacros = 1
 	wake.set()
 
 def reloadMacros():
 	global debug, prefix, macroFunctionNames, macroFunctions, menuFields
-	global _macroFunctionNames, _macroFunctions
+	global _macroFunctionNames, _macroFunctions, connected
+
+	if not connected:
+		return
 
 	success = 0
 	functions = []
@@ -284,12 +378,7 @@ def reloadMacros():
 		epics.caput(prefix+"caputRecorderUserMessage", "Macro file contains error(s)")
 
 	# erase menu strings
-	for menu in range(maxMacroMenus):
-		for field in menuFields:
-			if (field=="ZRST"):
-				epics.caput(prefix + ("caputRecorderMacros%d." % (menu+1)) + field, " ")
-			else:
-				epics.caput(prefix + ("caputRecorderMacros%d." % (menu+1)) + field, "")
+	epics.caput(prefix+"caputRecorderClearMacros", 1, wait=True, timeout=5)
 
 	if success:
 		functions = [o for o in getmembers(macros) if isfunction(o[1])]
@@ -311,6 +400,8 @@ def reloadMacros():
 			i += 1
 
 	selectMacro()
+	epics.caput(prefix+"caputRecorderUserMessage", "Macro (re)load succeeded")
+	epics.caput(prefix+"caputRecorderNeedRefresh", 1)
 	epics.caput(prefix+"caputRecorderReloadMacros", 0)
 
 
@@ -319,12 +410,20 @@ def reloadMacros():
 ## write args for selected macro to EPICS PVs
 
 def selectMacroMonFunc(pvname, value, char_value, **kwd):
-	global debug, doSelectMacro
+	global debug, doSelectMacro, connected
+
+	if not connected:
+		return
+
 	doSelectMacro = 1
 	wake.set()
 
 def selectMacro():
 	global debug, prefix, macroFunctionNames, macroFunctions, maxArgs, menuFields
+	global connected
+
+	if not connected:
+		return
 
 	# find which function is selected
 	funcName = epics.caget(prefix+"caputRecorderMacro")
@@ -336,9 +435,7 @@ def selectMacro():
 	if debug: print macroFunctionNames
 	if debug: print "func=", func
 	# erase arg names and values
-	for j in range(1,maxArgs+1):
-		epics.caput(prefix+("caputRecorderArg%dName" % j), "")
-		epics.caput(prefix+("caputRecorderArg%dValue" % j), "")
+	epics.caput(prefix + "caputRecorderClearArgs", "1")
 
 	# write arg names and values
 	if (func):
@@ -358,29 +455,28 @@ def selectMacro():
 	epics.caput(prefix+"caputRecorderMacrosBusy", 0)
 
 ########################################################################
-## Respond to monitors from ExecuteMacro, and AbortMacro PVs
-
-## execute selected macro
-def executeMacroMonFunc(pvname, value, char_value, **kwd):
-	global debug, doexecuteMacro
-	if debug: print "executeMacroMonFunc: value=%d" % value
-	if value:
-		doexecuteMacro = 1
-	wake.set()
+## Execute macro, respond to monitors from AbortMacro PV
 
 ## abort executing macro
 def abortMacroMonFunc(pvname, value, char_value, **kwd):
-	global debug, doAbortMacro, executingMacro
-	if debug: print "abortMacroMonFunc: value=%d" % value
+	global debug, doAbortMacro, executingMacro, connected
+
+	if not connected:
+		return
+
+	if debug: print "abortMacroMonFunc: value=%d\n" % value
 	if value:
 		doAbortMacro = 1
 		if executingMacro:
 			thread.interrupt_main()
-	wake.set()
+		wake.set()
 
 def executeMacro():
 	global debug, prefix, macroFunctionNames, macroFunctions, maxArgs, executingMacro
-	global _macroFunctionNames
+	global _macroFunctionNames, connected
+
+	if not connected:
+		return
 
 	# find which function is selected
 	funcName = epics.caget(prefix+"caputRecorderMacro")
@@ -426,13 +522,59 @@ def executeMacro():
 
 
 ############################################################################
+def startTimeMonFunc(pvname, value, char_value, **kwd):
+	global debug, startTime, wake, exitProgram, connected
+
+	if not char_value:
+		epics.caput(prefix+"caputRecorderStartTime", startTime)
+	else:
+		if startTime != char_value:
+			# Another instance of the program wrote a new start time.
+			# We don't ever want two copies of the program that use the same control PVs
+			# running simultaneously
+			exitProgram = 1
+			wake.set()
+
+def onConnectionChange(pvname=None, conn= None, **kws):
+	global debug, connected
+	if debug: print 'PV connection status changed: %s %s\n' % (pvname,  repr(conn))
+	if conn:
+		# wait for initial monitors to come in before allowing writer() to act on them
+		if connected == False:
+			#time.sleep(1)
+			connected = True
+	else:
+		connected = False
+		
+
+def heartbeat():
+	global debug, prefix, wake, exitProgram
+	maxDisconnectTime = 10
+	hb = epics.PV(prefix+"caputRecorderHeartbeat", connection_callback=onConnectionChange)
+	disconnectTime = 0
+	waitTime = 2
+	while True:
+		if epics.ca.isConnected(hb.chid):
+			disconnectTime = 0
+			hb.put(1)
+		else:
+			disconnectTime += waitTime
+			if disconnectTime > maxDisconnectTime:
+				if debug: print "caputRecorder: exiting"
+				exitProgram = 1
+				wake.set()
+		time.sleep(waitTime)
+
 def start():
 	global debug, prefix, doStartMacro, doStopMacro, doReloadMacros, doexecuteMacro
 	global doSelectMacro, doAbortMacro, executingMacro, msgQueue
 	global allowedUsers, forbiddenUsers, allowedHosts, forbiddenHosts
-	global commandMonitorList
+	global commandMonitorList, exitProgram
 
 	wake.clear()
+
+	# this monitor will kill us if a new copy of the program is executed
+	epics.camonitor(prefix+"caputRecorderStartTime",callback=startTimeMonFunc)
 
 	# Build lists of the PVs we'll monitor while recording
 	userPrefixes = epics.caget(prefix+"caputRecorderPrefixes", as_string=True)
@@ -452,11 +594,16 @@ def start():
 	users = epics.caget(prefix+"caputRecorderUsers", as_string=True)
 	(allowedUsers, forbiddenUsers) = calcAllowed(users)
 	if (debug):
-		print "allowedUsers='%s', forbiddenUsers='%s'\n" % (allowedUsers, forbiddenUsers)
+		print "allowedUsers='%s', forbiddenUsers='%s'" % (allowedUsers, forbiddenUsers)
 	hosts = epics.caget(prefix+"caputRecorderHosts", as_string=True)
 	(allowedHosts, forbiddenHosts) = calcAllowed(hosts)
 	if (debug):
-		print "allowedHosts='%s', forbiddenHosts='%s'\n" % (allowedHosts, forbiddenHosts)
+		print "allowedHosts='%s', forbiddenHosts='%s'" % (allowedHosts, forbiddenHosts)
+
+	# heartbeat
+	heartbeatThread = threading.Thread(target=heartbeat)
+	heartbeatThread.daemon = True
+	heartbeatThread.start()
 
 	# message and queue
 	msgQueue = Queue.Queue(maxsize=100)
@@ -489,6 +636,8 @@ def start():
 		if doAbortMacro:
 			doAbortMacro = 0
 			epics.caput(prefix+"caputRecorderAbortMacro", 0)
+		if exitProgram:
+			sys.exit()
 	stop()
 
 
@@ -503,8 +652,12 @@ def stop():
 	epics.camonitor_clear(prefix+"caputRecorderHosts")
 	epics.camonitor_clear(prefix+"caputRecorderPrefixes")
 
+def debugMonFunc(pvname, value, char_value, **kwd):
+	global debug
+	debug = value
+
 def go(argv=["xxx:"]):
-	global debug, prefix, commandMonitorList
+	global debug, prefix, commandMonitorList, startTime
 
 	usage = """
 	  python caputRecorder.py prefix [other_prefixes]
@@ -521,6 +674,10 @@ def go(argv=["xxx:"]):
 			commandMonitorList.append(otherprefix+"caputRecorderCommand")
 
 	if debug: print "initial commandMonitorList", commandMonitorList
+	debug = epics.caget(prefix+"caputRecorderDebug")
+	epics.camonitor(prefix+"caputRecorderDebug",callback=debugMonFunc)
+	startTime = time.strftime("%c")
+	epics.caput(prefix+"caputRecorderStartTime", startTime)
 	stop()
 	start()
 	stop()
